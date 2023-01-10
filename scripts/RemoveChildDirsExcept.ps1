82a9919f31220b2e8f4bf8f8eba898924d79503e
acidbubble <acidbubble@users.noreply.github.com>
2023-01-10 23:12:29 +0000

    Create RemoveChildDirsExcept.ps1

---
+++ scripts/RemoveChildDirsExcept.ps1
@@ -0,0 +1,27 @@
+Param(
+    [string]$BaseDir,
+    [string[]]$Exceptions
+)
+
+$TempDir = "$BaseDir-Temp";
+
+if (Test-Path $TempDir)
+{
+    Remove-Item -Path $TempDir -Recurse
+}
+
+New-Item -ItemType Directory -Path $TempDir -Force
+
+
+foreach ($exception in $Exceptions)
+{
+    #check if folder exists
+    if (Test-Path "$BaseDir/$exception")
+    {
+        Write-Host "Copying $BaseDir/$exception to $TempDir/$plugin"
+        Copy-Item -Path "$BaseDir/$exception" -Destination "$TempDir/$exception" -Recurse
+    }
+}
+
+Remove-Item -Path $BaseDir -Recurse -Force
+Rename-Item -Path $TempDir -NewName $BaseDir
