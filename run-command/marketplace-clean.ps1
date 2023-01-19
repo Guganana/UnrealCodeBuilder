1fd5112975e2422477ae9ef8f6a35a288946d8fb
acidbubble <acidbubble@users.noreply.github.com>
2023-01-19 16:08:50 +0000

    Update marketplace-clean.ps1

--- run-command/marketplace-clean.ps1
+++ run-command/marketplace-clean.ps1
@@ -4,9 +4,3 @@ Get-childitem -recurse -filter ".git*" -Force | Remove-Item -Force -Recurse -Ver
 Remove-Item -Recurse -Force ./Intermediate
 Remove-Item -Recurse -Force ./Binaries  
 Remove-Item -Recurse -Force ./*metadata -ErrorAction Ignore
-
-$upluginFiles = Get-ChildItem -Filter "*.uplugin" -Recurse -Force
-foreach ($Child in $upluginFiles)
-{
-    (Get-Content $Child.FullName) -Replace 'ENGINE_VERSION', "$env:UEVersion" | Set-Content $Child.FullName
-}
