1db634f8091562c6d20bbf6561e504f92b1cf3e8
acidbubble <acidbubble@users.noreply.github.com>
2023-02-13 16:26:38 +0000

    Update parse-metadata.ps1

--- run-command/parse-metadata.ps1
+++ run-command/parse-metadata.ps1
@@ -5,15 +5,30 @@ function SetEnvVar( $name, $value ) {
 	echo "$name=$value" >> $env:GITHUB_ENV;
 }
 
-Get-ChildItem -Path ./.metadata | ForEach-Object {
-	$varname = $_.Name
-	$value = Get-Content $_.Fullname -TotalCount 1
-
-	SetEnvVar $varname $value
-}
-
 SetEnvVar "releaseSHA" "$(git rev-parse HEAD)"
 SetEnvVar "releaseDate" "$(Get-Date -Format "yyyy, M, d")"
 SetEnvVar "UEVersion" "$UEVersion"
-SetEnvVar "releaseVersion" (Invoke-Expression """$env:releaseVersionFormat""")
-SetEnvVar "releaseName" (Invoke-Expression """$env:releaseNameFormat""")
+
+
+if (Test-Path -Path ./.metadata) 
+{
+	Get-ChildItem -Path ./.metadata | ForEach-Object {
+		$varname = $_.Name
+		$value = Get-Content $_.Fullname -TotalCount 1
+
+		SetEnvVar $varname $value
+	}
+	
+	SetEnvVar "releaseVersion" (Invoke-Expression """$env:releaseVersionFormat""")
+	SetEnvVar "releaseName" (Invoke-Expression """$env:releaseNameFormat""")
+}
+else
+{
+	Write-Warning "Unable to find .metadata folder in root directory of project"
+	Write-Warning "Will use default release version and format..."
+	
+	SetEnvVar "releaseName" "Placeholder"
+	SetEnvVar "releaseVersion" "Placeholder-$env:releaseSHA-@$UEVersion"
+}
+
+
